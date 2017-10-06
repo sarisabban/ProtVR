59720f25602a3904f0dab2b66d0206cecaa5d154
Sari Sabban <sari.sabban@gmail.com>
2017-10-07 00:48:39 +0300

    Delete launch.py

--- protvr/launch.py
+++
@@ -1,9 +0,0 @@
-import sys
-import logging
-
-
-def launch():
-    logging.basicConfig(stream=sys.stderr)
-    from protvr import app as application
-    # TODO: move this to an environment variable or somesuch
-    application.secret_key = 'vnmytyuyerwfsdfbyhfghjytrer'
